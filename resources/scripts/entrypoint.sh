2b0f4296b1ac9afcda135c6aad97f3732ccb2e4c
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-07-31 19:50:21 +0500

    New g4f version: 0.5.7.6 (current: 0.3.9.7) | pip install -U g4f

--- resources/scripts/entrypoint.sh
+++ resources/scripts/entrypoint.sh
@@ -3,7 +3,6 @@ python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
 uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir config
-celery -A config worker --loglevel=info &
 
 
 
