ff7007ed55a3d86adb13d4ae8dd54b8d30e7d48c
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-08-20 16:44:25 +0500

    New g4f version: 0.6.0.2 (current: 0.3.9.7) | pip install -U g4f

--- core/apps/api/enums/order.py
+++ core/apps/api/enums/order.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class OrderPaymentChoice(models.TextChoices):
     CLICK = "click", _("Click")
     PAYME = "payme", _("Payme")
+    CASH = "cash", _("Naqt pul")
     
     
 class OrderStatusChoice(models.TextChoices):
